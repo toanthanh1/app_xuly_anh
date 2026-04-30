bbb32158bc3dc0e9edbf51eb8d5d608dd1cc1bd2
toanthanh1 <toanhongngoc0853@gmail.com>
2026-04-30 23:02:12 +0700

    Add ImageView import to ChatAdapter.java

--- app/src/main/java/com/example/app_xhinh_anh/features/ai_assistant/ui/adapter/ChatAdapter.java
+++ app/src/main/java/com/example/app_xhinh_anh/features/ai_assistant/ui/adapter/ChatAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import com.example.app_xhinh_anh.R;
